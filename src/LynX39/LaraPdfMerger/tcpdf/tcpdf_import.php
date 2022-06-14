1aeeb3179aced9814fe918fe549c121d18161cf4
Vakhtang Rusieshvili <vr@arbeitsmedizin.de>
2022-06-14 14:38:52 +0200

    further package fixes

--- src/LynX39/LaraPdfMerger/tcpdf/tcpdf_import.php
+++ src/LynX39/LaraPdfMerger/tcpdf/tcpdf_import.php
@@ -43,7 +43,7 @@
  */
 
 // include the TCPDF class
-require_once(dirname(__FILE__).'/tcpdf.php');
+require_once('vendor/tecnickcom/tcpdf/tcpdf.php');
 // include PDF parser class
 require_once(dirname(__FILE__).'/tcpdf_parser.php');
 
